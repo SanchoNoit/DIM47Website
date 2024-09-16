adca6a6faf5c9941b9fd123a40c1768aee7fa0cd
James C. Noit <von.sancho@gmail.com>
2024-09-16 23:06:53 +0200

    Creado la base de javascript y botón para los test

--- Contents/Fundamentos_de_redes/scriptDeCuestionarioRedes.js
+++ Contents/Fundamentos_de_redes/scriptDeCuestionarioRedes.js
@@ -1,1 +1,6 @@
-document.getElementById("respuestaCorrecta").innerHTML.replace("Yiiija");
+function highlightCorrectAnswer () {
+    document.getElementById("respuestaCorrecta").style.color = 'green';
+    document.getElementById("respuestaErronea1").style.color = 'red';
+    document.getElementById("respuestaErronea2").style.color = 'red';
+    document.getElementById("respuestaErronea3").style.color = 'red';
+}
